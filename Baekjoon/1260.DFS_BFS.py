2a20b82064fe7be636648074bf330c8f8ec7fb10
Kimhyelyn <44598936+merry-hyelyn@users.noreply.github.com>
2023-04-13 00:16:56 +0900

    solve 1260

---
+++ Baekjoon/1260.DFS_BFS.py
@@ -0,0 +1,41 @@
+from collections import defaultdict, deque
+import copy
+
+
+def dfs(graph, root):
+    """stack"""
+    visited = [root]
+    stack = sorted(graph[root], reverse=True)
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+        stack += list(set(graph[node]) - set(visited))
+    return print(" ".join(list(map(str, visited))))
+
+
+def bfs(graph, root):
+    """queue"""
+    visited = [root]
+    queue = deque(graph[root])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+        queue += list(set(graph[node]) - set(visited))
+    return print(" ".join(list(map(str, visited))))
+
+
+n, m, r = map(int, input().split())
+dict_graph = defaultdict(list)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    start, end = map(int, input().split())
+    dict_graph[start].append(end)
+    dict_graph[end].append(start)
+    dict_graph[start].sort()
+    dict_graph[end].sort()
+
+dfs(copy.deepcopy(dict_graph), r)
+bfs(copy.deepcopy(dict_graph), r)
